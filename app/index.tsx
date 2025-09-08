5c7acc2f1b378a08666c3e52f8400baaa97e4b83
Felipe E. Schmidt <schmidtfe2@gmail.com>
2025-09-08 16:37:45 -0300

    feat: creating PokemonCard with loading and creating initial page to list them

---
+++ app/index.tsx
@@ -0,0 +1,62 @@
+import { FlatList, StyleSheet } from "react-native";
+
+import { usePokemonList } from "@/hooks/usePokemonList";
+import { Text, View } from "@/components/Themed";
+import { PokemonCard, PokemonCardLoading } from "@/components/ui/PokemonCard";
+import { useMemo } from "react";
+
+export default function ListScreen() {
+  const { pokemonData, loading, handleEndReached } = usePokemonList();
+
+  const hasData = useMemo(() => !!pokemonData.length, [pokemonData]);
+
+  const renderLoading = () => {
+    if (!loading) return null;
+    if (!hasData) {
+      return (
+        <FlatList
+          key={"pokemon-list-loading"}
+          data={Array.from({ length: 5 })}
+          renderItem={() => <PokemonCardLoading />}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          keyExtractor={(_, index) => `card-loading-${index}`}
+        />
+      );
+    }
+    return <PokemonCardLoading />;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>List of Pokémon</Text>
+      {!!hasData && (
+        <FlatList
+          key={"pokemon-list"}
+          data={pokemonData}
+          renderItem={({ item }) => <PokemonCard {...item} />}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          keyExtractor={(item, index) => `${item.name}-${item.id}-${index}`}
+          onEndReached={handleEndReached}
+        />
+      )}
+      {renderLoading()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+    gap: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 5,
+    height: 1,
+    width: "80%",
+  },
+});
---
+++ components/ui/PokemonCard.tsx
@@ -0,0 +1,77 @@
+import { Link } from "expo-router";
+import { View as UnthemedView, StyleSheet, useColorScheme } from "react-native";
+
+import { NamedAPIResource } from "@/types/pokemon";
+
+import { Text, View } from "../Themed";
+import Colors from "@/constants/Colors";
+import { Avatar } from "./Avatar";
+import { FavoriteButton } from "./FavoriteButton";
+
+export const PokemonCard = ({ id, name, url, sprite }: NamedAPIResource) => {
+  const colorScheme = useColorScheme();
+
+  const backgroundColor = Colors[colorScheme ?? "light"].cardBackground;
+  const textColor = Colors[colorScheme ?? "light"].cardText;
+  const principalColor = Colors[colorScheme ?? "light"].principal;
+
+  return (
+    <View style={[styles.card, { backgroundColor }]}>
+      <Avatar uri={sprite} style={styles.sprite} />
+      <UnthemedView style={styles.infoContainer}>
+        <Text style={[styles.title, { color: textColor }]}>{name}</Text>
+        <Link href={url} style={[styles.details, { color: principalColor }]}>
+          Press for details
+        </Link>
+      </UnthemedView>
+      <FavoriteButton isFavorite={Math.random() < 0.5} onPress={() => {}} />
+    </View>
+  );
+};
+
+export const PokemonCardLoading = () => {
+  const colorScheme = useColorScheme();
+
+  const backgroundColor = Colors[colorScheme ?? "light"].cardBackground;
+  const textColor = Colors[colorScheme ?? "light"].cardText;
+
+  return (
+    <View style={[styles.card, { backgroundColor }]}>
+      <Avatar style={styles.sprite} />
+      <UnthemedView style={styles.infoContainer}>
+        <Text style={[styles.title, { color: textColor }]}>Loading...</Text>
+      </UnthemedView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 8,
+    padding: 16,
+    elevation: 2,
+    gap: 24,
+  },
+  sprite: {
+    width: 64,
+    height: 64,
+  },
+  infoContainer: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+    gap: 4,
+  },
+  title: {
+    textTransform: "capitalize",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  details: {
+    fontSize: 14,
+  },
+});
