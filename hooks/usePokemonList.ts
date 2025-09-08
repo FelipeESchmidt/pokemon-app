a7a41ebc62b53c80ca11bd952cc57d14db1e2a5f
Felipe E. Schmidt <schmidtfe2@gmail.com>
2025-09-08 20:38:10 -0300

    feat: adding more functions to store and adding logic to list custom Pokemons in main list

--- hooks/usePokemonList.ts
+++ hooks/usePokemonList.ts
@@ -2,18 +2,29 @@ import { useEffect, useState } from "react";
 
 import { getPokemonList } from "@/api/pokeapi";
 import { NamedAPIResource } from "@/types/pokemon";
+import { useCustomStore } from "@/store/customPokemons";
 
 export const usePokemonList = () => {
+  const { getListAsSimple } = useCustomStore();
   const [page, setPage] = useState(1);
   const [hasNext, setHasNext] = useState(true);
   const [loading, setLoading] = useState(false);
   const [pokemonData, setPokemonData] = useState<NamedAPIResource[]>([]);
 
+  const handleSetPokemonData = (data: NamedAPIResource[], page: number) => {
+    const customData = getListAsSimple();
+    if (page === 1) {
+      setPokemonData([...customData, ...data]);
+      return;
+    }
+    setPokemonData((prev) => [...prev, ...data]);
+  };
+
   const handleFetchPokemonList = () => {
     setLoading(true);
     getPokemonList(page)
       .then(({ results, next }) => {
-        setPokemonData((prev) => [...prev, ...results]);
+        handleSetPokemonData(results, page);
         setHasNext(!!next);
       })
       .finally(() => setLoading(false));
--- store/customPokemons.ts
+++ store/customPokemons.ts
@@ -2,11 +2,15 @@ import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import type { Pokemon } from "../types/pokemon";
+import type { Pokemon, NamedAPIResource } from "../types/pokemon";
+import { Href } from "expo-router";
 
 type CustomState = {
   custom: Pokemon[];
   add: (p: Pokemon) => Pokemon;
+  get: (id: number) => Pokemon | undefined;
+  remove: (id: number) => void;
+  getListAsSimple: () => NamedAPIResource[];
 };
 
 export const useCustomStore = create<CustomState>()(
@@ -17,6 +21,18 @@ export const useCustomStore = create<CustomState>()(
         set({ custom: [pokemon, ...get().custom] });
         return pokemon;
       },
+      getListAsSimple: () => {
+        return get().custom.map(
+          (pokemon): NamedAPIResource => ({
+            id: pokemon.id,
+            name: pokemon.name,
+            url: ("custom/" + pokemon.id) as Href,
+            sprite: pokemon.sprites?.front_default,
+          })
+        );
+      },
+      get: (id) => get().custom.find((p) => p.id === id),
+      remove: (id) => set({ custom: get().custom.filter((p) => p.id !== id) }),
     }),
     {
       name: "custom-pokemon-store",
