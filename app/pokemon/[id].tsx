9fe74d59e0238b199620c4fe6bc29f942bd71ed4
Felipe E. Schmidt <schmidtfe2@gmail.com>
2025-09-09 18:31:30 -0300

    feat: updating pokemon route and hook to be able to handle locals and external pokemons

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,22 +1,33 @@
-import { StyleSheet, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Alert, StyleSheet, View } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
+import { useCustomStore } from "@/store/customPokemons";
 import { usePokemonDetailed } from "@/hooks/usePokemonDetailed";
 import {
   PokemonDetailedCard,
   PokemonDetailedCardLoading,
 } from "@/components/ui/PokemonDetailedCard";
+import { Button } from "@/components/ui/Button";
 
 export default function PokemonDetailsScreen() {
+  const router = useRouter();
   const { id } = useLocalSearchParams();
+  const { remove } = useCustomStore();
   const pokemonIdOrName = Array.isArray(id) ? id[0] : id;
 
   const { pokemonData, loading } = usePokemonDetailed(pokemonIdOrName);
+
+  const handleRemove = () => {
+    remove(pokemonIdOrName);
+    Alert.alert("Success", `Removed ${pokemonData?.name}`);
+    router.back();
+  };
 
   return (
     <View style={styles.container}>
       {loading && <PokemonDetailedCardLoading />}
       {pokemonData && <PokemonDetailedCard {...pokemonData} />}
+      {pokemonData?.__local && <Button onPress={handleRemove} text="Remove" />}
     </View>
   );
 }
--- hooks/usePokemonDetailed.ts
+++ hooks/usePokemonDetailed.ts
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
 
 import { getPokemonByIdOrName } from "@/api/pokeapi";
+import { useCustomStore } from "@/store/customPokemons";
 import { Pokemon } from "@/types/pokemon";
 
 export const usePokemonDetailed = (id: string) => {
+  const { get } = useCustomStore();
   const [loading, setLoading] = useState(false);
   const [pokemonData, setPokemonData] = useState<Pokemon>();
 
+  const handleSearchLocally = (id: string) => {
+    const localData = get(id);
+    if (localData) {
+      setPokemonData(localData);
+      return true;
+    }
+    return false;
+  };
+
   const handleFetchPokemonDetails = () => {
+    if (handleSearchLocally(id)) return;
     setLoading(true);
     getPokemonByIdOrName(id)
       .then((data) => setPokemonData(data))
