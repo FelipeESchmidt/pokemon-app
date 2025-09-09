e84145e347d778b9a5bbb42534aa125f704086da
Felipe E. Schmidt <schmidtfe2@gmail.com>
2025-09-09 19:02:35 -0300

    feat: adding a button to go to favorites page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,24 @@
 import { useMemo, useState } from "react";
 import { Link, useRouter } from "expo-router";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, useColorScheme } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
+import Colors from "@/constants/Colors";
 import { usePokemonList } from "@/hooks/usePokemonList";
 import { Text, View } from "@/components/Themed";
 import { InputSearch } from "@/components/ui/InputSearch";
 import { FloatingButton } from "@/components/ui/FloatingButton";
 import { PokemonCard, PokemonCardLoading } from "@/components/ui/PokemonCard";
+import { useFavoritesPokemons } from "@/store/favoritesPokemons";
 
 export default function ListScreen() {
+  const colorScheme = useColorScheme();
   const router = useRouter();
+  const { favorites } = useFavoritesPokemons();
   const [inputValue, setInputValue] = useState("");
   const { pokemonData, loading, handleEndReached } = usePokemonList();
+
+  const goldColor = Colors[colorScheme ?? "light"].gold;
 
   const handleInputBlur = () => {
     setInputValue("");
@@ -39,7 +45,18 @@ export default function ListScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>List of Pokémon</Text>
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>List of Pokémon</Text>
+        {favorites.length > 0 && (
+          <Link href="/favorites">
+            <FontAwesome
+              name="star-half-full"
+              size={25}
+              style={{ color: goldColor }}
+            />
+          </Link>
+        )}
+      </View>
       <InputSearch
         placeholder="Search by name or id"
         value={inputValue}
@@ -73,6 +90,12 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 12,
     gap: 8,
+  },
+  topContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     fontSize: 20,
